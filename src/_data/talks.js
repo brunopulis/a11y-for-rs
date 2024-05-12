9d652d9f8eeb05d136547d352f8bc6632ca1059e
Bruno Pulis <brunomendes.furtado@emeal.nttdata.com>
2024-05-12 02:32:38 -0300

    feat: add dinamyc speakers info

---
+++ src/_data/talks.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    speakerName: 'Victoria Krupp',
+    speakerImage: '',
+    presentationTitle: 'XPTO',
+    miniBio: 'Lorem Ipsum',
+    jobTitle: 'Analista de Qualidade',
+    company: 'CI&T'
+  },
+  {
+    speakerName: 'Bruno Pulis',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Elza Maria',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Alex Barboza',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Vitor David',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Mariana Pulis',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Reinaldo Ferraz',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Eduardo Lino',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Cadu Ribeiro',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+  {
+    speakerName: 'Doriane',
+    speakerImage: '',
+    presentationTitle: 'ABC',
+    miniBio: 'XYZ',
+    jobTitle: 'QA de acessibilidade',
+    company: 'NTT Data'
+  },
+]
