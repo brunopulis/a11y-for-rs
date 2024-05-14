0f5948c12d6c2ba41707dc7cbfea7936e93cc1c7
Bruno Pulis <brunomendes.furtado@emeal.nttdata.com>
2024-05-14 20:38:56 -0300

    feat: update images and presentation title

--- src/_data/talks.js
+++ src/_data/talks.js
@@ -1,55 +1,55 @@
 module.exports = [
   {
     speakerName: 'Elza Maria Albuquerque',
-    speakerTime: '10:00 - 10:30',
-    speakerImage: './images/speakers/speaker-1.jpg',
-    presentationTitle: 'Como podemos construir uma comunicação para todas as pessoas?',
-    presentationSummary: 'Para construirmos uma comunicação mais justa e equitativa, precisamos unir, compartilhar e aplicar as melhores práticas das áreas de diversidade, comunicação, acessibilidade e inclusão. Essa união colabora para tornar possível o acesso, melhorar a segurança e economia, dar visibilidade e representatividade para grupos minorizados, entre outros benefícios. Podemos construir um mundo melhor, um dia de cada vez, por meio da colaboração e do engajamento coletivo. O primeiro passo para eliminar barreiras na comunicação é conhecê-las, nomeá-las e buscar soluções eficazes. Com conscientização individual e coletiva, é possível criar possibilidades de diálogo, de conexão e de compreensão. Construir uma comunicação diversa, inclusiva e acessível é um processo contínuo que exige a participação da sociedade. Esta palestra tem o objetivo de apresentar esse contexto, compartilhar boas práticas e envolver as pessoas participantes na construção de uma comunicação mais justa, acolhedora e equitativa. Junte-se e faça parte dessa transformação!',
+    speakerTime: '10:00 às 10:30',
+    speakerImage: './images/speakers/elza.png',
+    presentationTitle: 'Como construir uma comunicação para todas as pessoas?',
+    presentationSummary: 'Você vai conhecer diversas boas práticas para construirmos uma comunicação mais diversa, inclusiva e acessível para todas as pessoas. A acessibilidade digital é um dos destaques para que isso aconteça. O convite é colaborarmos para um mundo melhor por meio dessa comunicação, um dia de cada vez. Vamos lá?',
     miniBio: "Comunicadora com mais de 20 anos de experiência, Fundadora da Maria Inclusiva, negócio de impacto social que busca eliminar barreiras na comunicação. Especialista em comunicação diversa, inclusiva e acessível para todas as pessoas.",
     presentationLink: ''
   },
   {
     speakerName: 'Reinaldo Ferraz',
-    speakerTime: '10:00 - 10:30',
-    speakerImage: './images/speakers/speaker-1.jpg',
-    presentationTitle: '"Pessoas idosas na web" ou "Acessibilidade e Realidade Estendida"',
-    presentationSummary: "Nessa palestra serão apresentados os principais desafios para acessibilidade em Realidade Virtual, bem como as principais necessidades e requisitos para o desenvolvimento de uma experiência imersível mais acessível.",
+    speakerTime: '10:30 às 11:00',
+    speakerImage: './images/speakers/reinaldo-ferraz.png',
+    presentationTitle: 'Pessoas idosas na web',
+    presentationSummary: "A população está envelhecendo. O que acontece, muitas vezes, é que cada vez mais pessoas enfrentam desafios relacionados à idade. Por isso, precisamos considerar a acessibilidade no dia a dia. Neste encontro, conheça as boas práticas que beneficiam esse grupo de pessoas usuárias que está aumentando cada vez mais e que, possivelmente, faremos parte dele.",
     miniBio: 'XYZ',
     presentationLink: ''
   },
   {
-    speakerName: 'Alex Frederico Ramos Barboza',
-    speakerTime: '11:00 - 11:30',
-    speakerImage: './images/speakers/speaker-1.jpg',
-    presentationTitle: 'Acessibilidade para Android - desafios e ferramentas.',
-    presentationSummary: 'Nesta palestra iremos entender as barreiras enfrentadas por usuários com deficiências visuais ao lidar com aplicações móveis, as causas desses problemas e quais as ferramentas disponíveis para desenvolvedores Android a fim de mitigar os problemas de acessibilidade.',
+    speakerName: 'Alex Barboza',
+    speakerTime: '11:00 às 11:30',
+    speakerImage: './images/speakers/alex-barboza.png',
+    presentationTitle: 'Acessibilidade para Android - desafios e ferramentas',
+    presentationSummary: 'Durante a palestra, você vai conhecer as barreiras enfrentadas por pessoas usuárias com deficiências visuais ao lidar com aplicações móveis. Além disso, você vai saber as causas desses problemas e quais são as ferramentas disponíveis para pessoas desenvolvedoras Android com o objetivo de minimizar os problemas de acessibilidade.',
     miniBio: 'Sou técnico em Informática pelo Centro Paula Souza, Bacharel em Sistemas de Informação pela USP e atualmente mestrando em Engenharia de Software também pela USP. Meu tema de pesquisa é acessibilidade para aplicações móveis, com foco em técnicas e ferramentas para identificação automática de violações de acessibilidade. Na indústria, atuo como desenvolvedor mobile há 7 anos. No Brasil, fui desenvolvedor Android no Itaú. Em Portugal, trabalhei como desenvolvedor Android para a Aubay nas áreas financeira e de telecomunicações. Atualmente trabalho também como dev Android para a Swiss Post (os correios da Suíça)',
     presentationLink: ''
   },
   {
     speakerName: 'Vitor David',
-    speakerTime: '11:30 - 12:00',
-    speakerImage: './images/speakers/speaker-1.jpg',
+    speakerTime: '11:30 às 12:00',
+    speakerImage: './images/speakers/vitor-david.png',
     presentationTitle: 'Processo e métricas de acessibilidade',
-    presentationSummary: 'A ideia é falar um pouco sobre o processo de acessibilidade em times de produtos e como metrificar a acessibilidade',
+    presentationSummary: 'Nesta palestra, você vai aprender mais sobre o processo de acessibilidade em times de produtos e como metrificar a acessibilidade.',
     miniBio: 'Designer formado a uns 8 anos, com MBA em Mídias Sociais e cursando MBA em Ux pela Puc. Atuei em projetos de acessibilidade envolvendo Grupo pão de açúcar, Hand Talk, Bradesco Seguros e outros.',
     presentationLink: ''
   },
   {
     speakerName: 'Bruno Pulis',
-    speakerTime: '14:00 - 14:30',
-    speakerImage: './images/speakers/speaker-1.jpg',
+    speakerTime: '14:00 às 14:30',
+    speakerImage: './images/speakers/bruno-pulis.png',
     presentationTitle: 'Uma visão sobre testes de acessibilidade',
-    presentationSummary: 'Nessa palestra irei demonstrar algumas abordagens de testes automatizados para acessibilidade e como podemos integrar acessibilidade dentro da pirâmide de testes.',
+    presentationSummary: 'Nessa palestra, Bruno vai demonstrar algumas abordagens de testes automatizados para acessibilidade. Além disso, ele vai contar como podemos integrar acessibilidade dentro da pirâmide de testes.',
     miniBio: 'Desenvolvedor de software especialista em acessibilidade web, formado pela Faculdade Pitágoras, com 15 anos de experiência. Apaixonado por acessibilidade, criou o Awesome A11y. Curioso. Viciado em Star Wars e café.',
     presentationLink: ''
   },
   {
     speakerName: 'Mariana Pulis',
-    speakerTime: '14:30 - 15:00',
-    speakerImage: './images/speakers/speaker-1.jpg',
+    speakerTime: '14:30 às 15:00',
+    speakerImage: './images/speakers/mariana-pulis.png',
     presentationTitle: 'Comunicação acessível e Libras',
-    presentationSummary: 'Vamos conversar sobre surdez, Libras e formas de comunicação para que todos tenham equidade de apredizagem, informação e lazer.',
+    presentationSummary: 'Nesta palestra, vamos conversar sobre surdez, Libras e formas de comunicação para que todas as pessoas tenham equidade de aprendizagem, informação e lazer.',
     miniBio: 'Sou Mariana Pulis, tenho 31 anos, natural de Petrópolis-RJ, residente em BH. Trabalho como analista de testes de acessibilidade digital e sou intérprete de Libras.',
     presentationLink: ''
   },
@@ -58,16 +58,16 @@ module.exports = [
     speakerTime: '15:00 - 15:30',
     speakerImage: './images/speakers/speaker-1.jpg',
     presentationTitle: '70% pessoas, 30% técnico: O lado humano do design acessível',
-    presentationSummary: 'Junte-se a nós para uma conversa descontraída com Eduardo e Cadu, onde eles vão compartilhar suas vivências sobre a importância do toque humano na criação de projetos acessíveis. Descubra como essa perspectiva mudou suas vidas e como a acessibilidade se tornou parte do seu dia a dia. Vamos explorar juntos como integrar esse valor essencial em nossos próprios projetos e na sociedade.',
+    presentationSummary: 'Junte-se a nós para uma conversa descontraída com Eduardo e Cadu! Eles vão compartilhar suas vivências sobre a importância do toque humano na criação de projetos acessíveis. Descubra como essa perspectiva mudou vidas e como a acessibilidade se tornou parte do dia a dia dos palestrantes. Durante o encontro, vamos explorar, juntos, como integrar a acessibilidade em nossos próprios projetos e na sociedade.',
     miniBio: 'Eduardo Zangrossi Lino: Product Designer com mais de 11 anos de experiência na indústria tech e com foco em Acessibilidade Web e Digital e experiência em Desenvolvimento Front-end, integra equipes multidisciplinares de design e desenvolvimento durante todo o ciclo de criação e growth de produtos digitais, atuando como facilitador para uma comunicação mais eficaz entre as equipes e introduzindo soluções inovadoras e inclusivas. Cadu Ribeiro: Designer, professor e pesquisador. Mestre em Design pela PUC-Rio e especialista em Ergodesign, usabilidade e interação humano-computador pela mesma instituição. Já contribuiu para diversas startups no Rio de Janeiro, São Paulo e Portugal montando times de UX e liderando equipes, sempre com foco em projetos inclusivos.',
     presentationLink: ''
   },
   {
     speakerName: 'Doriane Vasconcelos',
     speakerTime: '15:30 - 16:30',
-    speakerImage: './images/speakers/speaker-1.jpg',
-    presentationTitle: 'Importância da audiodescrição Nos espaços corporativos',
-    presentationSummary: 'Esse recurso promove inclusão no trabalho. Estimula a atuação, participação, interação e Protagonismo Das pessoas com deficiência visual Bem como demais públicos da audiodescrição. Considero um tema muito relevante dentro das organizações. Afinal, dentro delas podemos Perceber a importância em todos os processos de gestão, Educação, cultural, Socialização, comercial,  Saúde, segurança do trabalho, qualidade de vida, Comunicação, marketing,  Tudo isso em Espaço físico ou digital.',
+    speakerImage: './images/speakers/doriane-vasconcelos.png',
+    presentationTitle: 'A importância da audiodescrição nos espaços corporativos',
+    presentationSummary: 'Durante o encontro, vamos falar sobre a audiodescrição e como ela colabora para promover inclusão no trabalho. É um recurso que estimula a atuação, a participação, a interação e o protagonismo das pessoas com deficiência visual e de outros públicos que o acessam.',
     miniBio: 'Bacharela em Administração com Especialização em Gestão de Pessoas pelo IFBA, Bacharelanda Interdisciplinar em Humanidades com Especialização em Estudos Jurídicos na UFBA, empregada pública na Embasa, Consultora Técnica e Cognitiva em Audiodescrição,Acessibilidade e Inclusão e, defendo a diversidade humana nas organizações. Sou colaboradora em projetos da UFRJ, Fiocruz, IFBA, IFRJ e Movimento WEB Para Todos. Por 18 meses participei de projeto na UNESP - Bauru. Sou Fundadora e Coordenadora do coletivo: Consultores em Audiodescrição Brasil e da @equipead_dorianevasconcelos. Além disso, membra do Comitê de Diversidade, Equidade e Inclusão na Embasa. Atuo como Formadora, Pesquisadora, Escritora, Palestrante, Criadora de Conteúdo digital.',
     presentationLink: ''
   }
